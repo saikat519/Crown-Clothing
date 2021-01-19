fba05ec52fdb6190a99856939e9393275ee6b5a8
saikat519 <saikat.mukhopadhyay17@egmail.com>
2021-01-19 16:13:10 +0530

    router logic added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import HomePage from './pages/Homepage/Homepage'
+import { BrowserRouter,Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Menu-item/Menu-item.jsx
+++ src/components/Menu-item/Menu-item.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import { useHistory } from 'react-router-dom'
 import './Menu-item.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size }) => {
+  
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push(`/${title}`)
+  }
+
+   return <div className={`${size} menu-item`} onClick={handleClick} >
     <div
       className='background-image'
       style={{
@@ -15,6 +22,6 @@ const MenuItem = ({ title, imageUrl, size }) => (
       <span className='subtitle'>SHOP NOW</span>
     </div>
   </div>
-);
+};
 
 export default MenuItem;
