59666f1f077ea12822ced9cd5634020c4512e2d0
Saikat Mukhopadhyay <49401892+saikat519@users.noreply.github.com>
2021-01-26 17:39:57 +0530

    hide credentials

--- src/firebase.util.js
+++ src/firebase.util.js
@@ -3,13 +3,7 @@ import firebase from 'firebase'
 
 /// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyByfFEiK2U-Hvd4o7P2QXqBjattvgabWXQ",
-    authDomain: "crown-clothing-cbf3c.firebaseapp.com",
-    projectId: "crown-clothing-cbf3c",
-    storageBucket: "crown-clothing-cbf3c.appspot.com",
-    messagingSenderId: "256970681251",
-    appId: "1:256970681251:web:18f5e700e0b2ee20610816",
-    measurementId: "G-MVHT4GXN7J"  
+    //Your firebase credentials
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
